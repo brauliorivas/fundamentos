12dd053421125da6614900337f220626ac111c0f
brauliorivas <brauliorivas2004@gmail.com>
2021-01-10 16:13:48 -0500

    Entender el tiepo en javascript

--- prototipo.js
+++ prototipo.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-useless-constructor */
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-plusplus */
@@ -14,45 +16,50 @@
 /* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
 
-function heredar(prototipoHijo, prototipoPadre) {
-    var Fn = function () {};
-    Fn.prototype = prototipoPadre.prototype;
-    // eslint-disable-next-line new-parens
-    prototipoHijo.prototype = new Fn;
-    prototipoHijo.prototype.constructor = prototipoHijo;
-}
-function Persona(nombre, apellido, altura) {
-    this.nombre = nombre;
-    this.apellido = apellido;// this hace referancia a si mismo
-    this.altura = altura;
-}
+class Persona {
+    constructor(nombre, apellido, altura) {
+        this.nombre = nombre;
+        this.apellido = apellido;// this hace referancia a si mismo
+        this.altura = altura;
+    }
 
-Persona.prototype.saludar = function () { // prototype, hace referencia al objeto
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
-    // saludar es un metodo, del prototipo de tipo persona
-};
-// Persona.prototype.soyAlto = function () {
-//     if (this.altura > 1.8) {
-//         console.log(`Soy alto porque mido ${this.altura} metros`);
-//     } else {
-//         console.log(`Soy bajo porque mido ${this.altura} metros`);
-//     }
-// };
-Persona.prototype.soyAlto = function () {
-    return this.altura > 1.8; // tener cuidado con this y apuntar a window y no al this.obejto.prot
-};
-function Desarrollador(nombre, apellido) {
-    this.nombre = nombre;
-    this.apellido = apellido;
+    saludar(fn) {
+        var { nombre, apellido } = this;
+        console.log(`Hola, me llamo ${nombre} ${apellido}`);
+        if (fn) {
+            fn(nombre, apellido, false);
+        }
+    } // saludar es un metodo, del prototipo de tipo persona
+
+    soyAlto() {
+        return this.altura > 1.8;
+    }// tener cuidado con this y apuntar a window y no al this.obejto.prot
 }
 
-heredar(Desarrollador, Persona);
+class Desarrollador extends Persona { // usamos extends para heredar propiedades de Persona
+    constructor(nombre, apellido, altura) {
+        super(nombre, apellido, altura);// super para indicar que se construye igual que el padre
+    }
 
-Desarrollador.prototype.saludar = function () {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`);
-};
-// new crea un nuevo objeto, se le asigna el prototipo Persona()
+    saludar(fn) { // pasamos una funcion como parametro, no poner ()
+        var { nombre, apellido } = this;
+        console.log(`Hola, me llamo ${nombre} ${apellido} y soy desarrollador`);
+        if (fn) {
+            fn(nombre, apellido, true);
+        }
+    }
+}
+ function responderSaludo(nombre, apellido, esDev) { // se ejecuta con el metodo saludar
+     console.log(`Buen día ${nombre} ${apellido}`);
+     if (esDev) {
+         console.log(`Ah mirá, no sabia que eras desarrollador`);
+     }
+ }
+// new crea un nuevo objeto, se le asigna el prototipo o clase Persona
 var braulio = new Persona("Braulio", "Rivas", 1.60); // braulio es un obejeto, de prototipo Persona
-var cesar = new Persona("Cesar", "Ampuero", 1.70);
 var henrique = new Persona("Henríque", "Hernández", 1.90);
-var juan = new Desarrollador("Juan", "Gómez");
+var juan = new Desarrollador("Juan", "Gómez", 1.80);
+
+braulio.saludar();
+henrique.saludar(responderSaludo);// se responder a si solo
+juan.saludar(responderSaludo);// el parametro de funcion es responderSaludo
